2f826939eeec22499099250ddcf0e6bb6cda84cd
unknown <inesachour@insat.u-carthage.tn>
2022-03-15 15:10:24 +0100

    removed extra module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { QuoteModule } from './quote/quote.module';
-import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [
     QuoteModule,
     MongooseModule.forRoot(
-      'mongodb+srv://admin:??@cluster0.gbprk.mongodb.net/quotesdb?retryWrites=true&w=majority',
+      'mongodb+srv://admin:@cluster0.gbprk.mongodb.net/quotesdb?retryWrites=true&w=majority',
     ),
-    CategoryModule,
   ],
   controllers: [AppController],
   providers: [AppService],
